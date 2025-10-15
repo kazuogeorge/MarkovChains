f5786d03885ca90faba9dea6d291c6e9b3d18d26
kazuogeorge <kazuogeorge@gmail.com>
2025-10-15 14:17:52 -0400

    Update MarkovShort.py

--- MarkovShort.py
+++ MarkovShort.py
@@ -1,16 +1,16 @@
 import time
-peeps = int(input("how many people are playing? "))
-num = int(input("how many iterations? "))
-chancel = [0] + [1/(peeps-1)]*(peeps-1)
+peeps = int(input("how many people are playing? ")) #User inputs the number of people
+num = int(input("how many iterations? ")) #User inputs the number of iterations
+chancel = [0] + [1/(peeps-1)]*(peeps-1) #Creates a list with the chance of each position after the first round
 
-stime = time.perf_counter()
+stime = time.perf_counter() #records the time before the start of the iterations
 for i in range(num):
-    newl = [0] * peeps
-    for x in range(1,len(l)):
-        newl[x]=l[0]*1/(peeps-1)
-    for x in range(1,len(l)):
-        for b in range(x):
-            newl[b]+=l[x]/x
-    chancel = newl
-print(time.perf_counter() - stime)
-print(l)
+    newl = [0] * peeps  # Creates a transition list with the right number of indexes for the people
+    for x in range(1,len(chancel)):
+        newl[x]=chancel[0]*1/(peeps-1) #The number 1 person gives their chance evenly across the rest of the people
+    for x in range(1,len(chancel)):
+        for b in range(x): #Each other person gives their chance evenly across the rest of the people with numbers lower than them
+            newl[b]+=chancel[x]/x
+    chancel = newl #writes the transition list to the main list
+print("runtime: "+str(time.perf_counter() - stime)) #prints the time that it took to iterate
+print(chancel)
