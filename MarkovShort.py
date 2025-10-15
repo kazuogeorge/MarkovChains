4b37673fb03560192cf54744d8624af047e2138f
kazuogeorge <kazuogeorge@gmail.com>
2025-10-15 14:09:29 -0400

    Update MarkovShort.py

--- MarkovShort.py
+++ MarkovShort.py
@@ -1,16 +1,16 @@
 import time
 peeps = int(input("how many people are playing? "))
 num = int(input("how many iterations? "))
-l = [0] + [1/(peeps-1)]*(peeps-1)
+chancel = [0] + [1/(peeps-1)]*(peeps-1)
 
 stime = time.perf_counter()
 for i in range(num):
-    ln = [0] * peeps
+    newl = [0] * peeps
     for x in range(1,len(l)):
-        ln[x]=l[0]*1/(peeps-1)
+        newl[x]=l[0]*1/(peeps-1)
     for x in range(1,len(l)):
         for b in range(x):
-            ln[b]+=l[x]/x
-    l = ln
+            newl[b]+=l[x]/x
+    chancel = newl
 print(time.perf_counter() - stime)
 print(l)
