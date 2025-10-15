cebc37af7d8944ec7d9773a932cb1a30571531ce
kazuogeorge <kazuogeorge@gmail.com>
2025-10-15 12:41:25 -0400

    Update Markovdiscrete.py

--- Markovdiscrete.py
+++ Markovdiscrete.py
@@ -1,15 +1,14 @@
 import random
 focus = 0
-peeps = int(input("how many people are playing? "))
-num = int(input("how many rounds? "))
-l = [0]*peeps
-for x in range(0,num):
+peeps = int(input("how many people are playing? ")) #User inputs the amount of people
+num = int(input("how many rounds? ")) #User inputs the amount of people
+countl = [0]*peeps #Create string to count the amount of times the focus was on each person
+for x in range(0,num): #repeat for the specified number of rounds
     if focus == 0:
-        focus = random.randint(1,peeps-1)
+        focus = random.randint(1,peeps-1) #The number 1 person chooses a random person
     else:
-        focus = random.randint(0,a-1)
-    l[focus] += 1
-print(l)
-for x in range(0,len(l)):
-    l[x-1] /= (.01*num)
-print(l)
+        focus = random.randint(0,a-1) #The person chooses a random person with a lower number than them
+    countl[focus] += 1 #Adds one at the index on the counting list
+for x in range(0,len(countl)):  #Normalizes the count of each index so it represnted as a percent/100
+    countl[x-1] /= (.01*num)
+print(countl) # prints the output
