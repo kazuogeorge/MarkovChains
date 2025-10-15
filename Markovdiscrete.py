06a1510911f5293c5ddc7d1a6c4f0de2b99541b7
kazuogeorge <kazuogeorge@gmail.com>
2025-10-15 12:30:20 -0400

    Update Markovdiscrete.py

--- Markovdiscrete.py
+++ Markovdiscrete.py
@@ -1,14 +1,14 @@
 import random
-a = 0
+focus = 0
 peeps = int(input("how many people are playing? "))
 num = int(input("how many rounds? "))
 l = [0]*peeps
 for x in range(0,num):
-    if a == 0:
-        a = random.randint(1,peeps-1)
+    if focus == 0:
+        focus = random.randint(1,peeps-1)
     else:
-        a = random.randint(0,a-1)
-    l[a] += 1
+        focus = random.randint(0,a-1)
+    l[focus] += 1
 print(l)
 for x in range(0,len(l)):
     l[x-1] /= (.01*num)
