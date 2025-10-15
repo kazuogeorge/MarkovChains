9e6d2bce19d619b59c91c181d3073f4c072453ed
kazuogeorge <kazuogeorge@gmail.com>
2025-10-15 12:54:44 -0400

    Update Markovdiscrete.py

--- Markovdiscrete.py
+++ Markovdiscrete.py
@@ -1,8 +1,10 @@
 import random
+import time
 focus = 0
 peeps = int(input("how many people are playing? ")) #User inputs the amount of people
 num = int(input("how many rounds? ")) #User inputs the amount of people
 countl = [0]*peeps #Create string to count the amount of times the focus was on each person
+stime = time.perf_counter() #records the time before the start of the iterations
 for x in range(0,num): #repeat for the specified number of rounds
     if focus == 0:
         focus = random.randint(1,peeps-1) #The number 1 person chooses a random person
@@ -11,4 +13,5 @@ for x in range(0,num): #repeat for the specified number of rounds
     countl[focus] += 1 #Adds one at the index on the counting list
 for x in range(0,len(countl)):  #Normalizes the count of each index so it represnted as a percent/100
     countl[x-1] /= (.01*num)
+print("runtime: "+str(time.perf_counter() - stime)) #prints the time that it took to iterate
 print(countl) # prints the output
